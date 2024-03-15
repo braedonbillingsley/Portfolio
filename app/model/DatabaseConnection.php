1c2faa4adab84ede74b1da44976c6259c28fc1ab
Braedon Billingsley <Billingsley.Braedon@student.greenriver.edu>
2024-03-15 14:16:07 -0400

    Set up PHP MVC structure, installed composer, updated .gitignore and .htaccess. Added project images and DatabaseConnection.php for instantiating PDO database connection objects securely.

---
+++ app/model/DatabaseConnection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *  This is the DATABASE CONNECTION for MetaDirect database access*
+ *  @authors Mehak Saini, Braedon Billingsley, Jack Le
+ *  @copyright 2024
+ *  @url https://bbillingsley.greenriverdev.com/328/MetaDirect
+ */
+require_once($_SERVER['DOCUMENT_ROOT'] .'/../portfolio-db-config.php');
+class DatabaseConnection
+{
+    private static ?PDO $dbh = null;
+    private function __construct() {}
+
+    /**
+     *  Get the database connection.
+     *  This method establishes a connection to the database using PDO.
+     *  @return PDO The PDO object representing the database connection.
+     *  @throws PDOException Throws a PDOException if there is an error connecting to the database.
+     */
+    public static function getConnection(): PDO
+    {
+        if (is_null(self::$dbh))
+        {
+            try
+            {
+                /* @noinspection PhpUndefinedConstantInspection */
+                self::$dbh = new PDO(dsn: DB_DSN, username: DB_USERNAME, password: DB_PASSWORD);
+            }
+
+            catch (PDOException $e)
+            {
+                echo "Database connection error: " . $e->getMessage();
+                throw $e;
+            }
+        }
+
+        return self::$dbh;
+    }
+}
